ef73986cfc5a05798568b3d7636e07356543d517
Y********parsa-sbg <parsa.sbg@gmail.com>
2024-08-03 17:51:03 +0330

    refactor: move weather data processing to WeatherContext

--- src/contexts/weatherContext.js
+++ src/contexts/weatherContext.js
@@ -1,25 +1,51 @@
 import React, { createContext, useContext, useState } from 'react'
+import moment from 'moment';
 
-const useWeather = () =>  useContext(WeatherContext) 
+
+const useWeather = () => useContext(WeatherContext)
 
 const WeatherContext = createContext(null)
 
 const WeatherProvider = ({ children }) => {
     const [cityName, setCityName] = useState('tehran');
     const [weatherData, setWeatherData] = useState(null);
-    const [isLoading, setIsLoading] = useState (true)
+    const [isLoading, setIsLoading] = useState(true)
+
+    const changeKelvinToCdegree = kelvin => Math.floor(kelvin - 272.15)
 
     const fetchWeather = async (cityName) => {
         setIsLoading(true)
-        const res =  await fetch (`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=40846d7c3078163c44131acde6033488`)
+        const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=40846d7c3078163c44131acde6033488`)
         const data = await res.json()
         
         res.status == 200 && setWeatherData(data)
         setIsLoading(false)
     }
 
+    const mainWeatherInfo = {
+        cityName: weatherData?.name,
+        temp: changeKelvinToCdegree(weatherData?.main.temp),
+        date: moment().format('h:mm a - dddd')
+    }
+
+    const moreWheaherInfo = {
+        maxTemp: changeKelvinToCdegree(weatherData?.main.temp_max),
+        minTemp: changeKelvinToCdegree(weatherData?.main.temp_min),
+        Humidity: weatherData?.main.humidity,
+        Cloudy: weatherData?.clouds.all,
+        WindSpeed: weatherData?.wind.speed
+
+    }
+
     return (
-        <WeatherContext.Provider value={{weatherData , fetchWeather, isLoading}}>
+        <WeatherContext.Provider
+            value={{
+                mainWeatherInfo,
+                moreWheaherInfo,
+                fetchWeather,
+                isLoading
+            }}>
+
             {children}
         </WeatherContext.Provider>
     )
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,31 +3,13 @@ import './Home.css'
 import MainWeatherInfo from '../../components/MainWeatherInfo/MainWeatherInfo'
 import MoreWeatherInfo from '../../components/MoreWeatherInfo/MoreWeatherInfo'
 import { useWeather, WeatherContext } from '../../contexts/weatherContext'
-import moment from 'moment';
 import Loader from '../../components/Loader/Loader'
 import Container from '@mui/material/Container';
 
 
 export default function Home() {
 
-  const { weatherData, isLoading } = useWeather()
-
-  const changeKelvinToCdegree = kelvin => Math.floor(kelvin - 272.15)
-
-  const mainWeatherInfo = {
-    cityName: weatherData?.name,
-    temp: changeKelvinToCdegree(weatherData?.main.temp),
-    date: moment().format('h:mm a - dddd')
-  }
-
-  const moreWheaherInfo = {
-    maxTemp: changeKelvinToCdegree(weatherData?.main.temp_max),
-    minTemp: changeKelvinToCdegree(weatherData?.main.temp_min),
-    Humidity: weatherData?.main.humidity,
-    Cloudy: weatherData?.clouds.all,
-    WindSpeed: weatherData?.wind.speed
-
-  }
+  const { isLoading ,mainWeatherInfo, moreWheaherInfo} = useWeather()
 
   return (
     <section className='home'>
