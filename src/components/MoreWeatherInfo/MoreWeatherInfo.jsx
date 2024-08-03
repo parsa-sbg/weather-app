1777c2a3dbfba52d12a6f264d6c2be5894615b6f
Y********parsa-sbg <parsa.sbg@gmail.com>
2024-08-03 15:47:25 +0330

    style: improve styling of the Home page

--- src/components/MoreWeatherInfo/MoreWeatherInfo.jsx
+++ src/components/MoreWeatherInfo/MoreWeatherInfo.jsx
@@ -9,16 +9,19 @@ import { FaWind } from "react-icons/fa";
 
 
 
-export default function MoreWeatherInfo({moreWheaherInfo}) {
+export default function MoreWeatherInfo({ moreWheaherInfo }) {
   return (
-    <div className='MoreWeatherInfo'>
-      <span className='MoreWeatherInfo__title'>Weather Details...</span>
-      <div className='MoreWeatherInfo__body'>
-        <MoreWeatherInfoItem name='Temp max' value={moreWheaherInfo.maxTemp+'°'} icon={<FaTemperatureThreeQuarters size={30} color='red'/>}/>
-        <MoreWeatherInfoItem name='Temp min' value={moreWheaherInfo.minTemp+'°'} icon={<FaTemperatureThreeQuarters size={30} color='blue'/>}/>
-        <MoreWeatherInfoItem name='Humidity' value={moreWheaherInfo.Humidity+'%'} icon={<WiHumidity size={30}/>}/>
-        <MoreWeatherInfoItem name='Cloudy' value={moreWheaherInfo.Cloudy+'%'} icon={<BsFillCloudsFill size={30}/>}/>
-        <MoreWeatherInfoItem name='Wind' value={moreWheaherInfo.WindSpeed+' m/s'} icon={<FaWind size={30}/>}/>
+
+    <div className='MoreWeatherInfo-wrapper'>
+      <div className='MoreWeatherInfo'>
+        <span className='MoreWeatherInfo__title'>Weather Details...</span>
+        <div className='MoreWeatherInfo__body'>
+          <MoreWeatherInfoItem name='Temp max' value={moreWheaherInfo.maxTemp + '°'} icon={<FaTemperatureThreeQuarters size={30} color='#DFA1A1' />} />
+          <MoreWeatherInfoItem name='Temp min' value={moreWheaherInfo.minTemp + '°'} icon={<FaTemperatureThreeQuarters size={30} color='#6D97CA' />} />
+          <MoreWeatherInfoItem name='Humidity' value={moreWheaherInfo.Humidity + '%'} icon={<WiHumidity size={30} />} />
+          <MoreWeatherInfoItem name='Cloudy' value={moreWheaherInfo.Cloudy + '%'} icon={<BsFillCloudsFill size={30} />} />
+          <MoreWeatherInfoItem name='Wind' value={moreWheaherInfo.WindSpeed + ' m/s'} icon={<FaWind size={30} />} />
+        </div>
       </div>
     </div>
   )
