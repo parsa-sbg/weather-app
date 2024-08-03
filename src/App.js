2808c75f12ecba0190a7cb9379d60910014723ee
Y********parsa-sbg <parsa.sbg@gmail.com>
2024-08-03 12:07:21 +0330

    feat: fetch default weather data for Tehran on page load

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import Header from './components/Header/Header';
 import Container from '@mui/material/Container';
 import Routes from './routes';
 import { useWeather } from './contexts/weatherContext'
+import { useEffect } from 'react';
 
 function App() {
+  const {fetchWeather} = useWeather()
 
-
-
+  useEffect(() => {
+    fetchWeather('tehran')
+  }, [])
+  
   return (
     <div className="App">
       <Container>
