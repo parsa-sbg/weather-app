a0d57906961ad8d1a167be9db670a8806315dc8c
Y********parsa-sbg <parsa.sbg@gmail.com>
2024-08-03 12:30:15 +0330

    feat: create Loader component for displaying loading state

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Loader.css'
+
+export default function Loader() {
+  return (
+    <div className='laoder-wrapper'>
+        <div className='loader'></div>
+    </div>
+  )
+}
