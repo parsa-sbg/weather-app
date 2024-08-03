098527ef4d59080639b846b5e9052e1bd4bc1cb0
Y********parsa-sbg <parsa.sbg@gmail.com>
2024-08-03 12:39:24 +0330

    feat: add isLoading state and related functions to WeatherContext

--- src/contexts/weatherContext.js
+++ src/contexts/weatherContext.js
@@ -7,16 +7,19 @@ const WeatherContext = createContext(null)
 const WeatherProvider = ({ children }) => {
     const [cityName, setCityName] = useState('tehran');
     const [weatherData, setWeatherData] = useState(null);
+    const [isLoading, setIsLoading] = useState (true)
 
     const fetchWeather = async (cityName) => {
+        setIsLoading(true)
         const res =  await fetch (`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=40846d7c3078163c44131acde6033488`)
         const data = await res.json()
         
         res.status == 200 && setWeatherData(data)
+        setIsLoading(false)
     }
 
     return (
-        <WeatherContext.Provider value={{weatherData , fetchWeather}}>
+        <WeatherContext.Provider value={{weatherData , fetchWeather, isLoading}}>
             {children}
         </WeatherContext.Provider>
     )
